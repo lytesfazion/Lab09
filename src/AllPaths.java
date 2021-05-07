ba9bd610f34ff6257422ed0a599b93b967849863
lytesfazion <liwamtesfazion@gmail.com>
2021-05-06 23:29:55 -0700

    submission commit

--- src/AllPaths.java
+++ src/AllPaths.java
@@ -1,60 +1,123 @@
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Queue;
+import java.util.Stack;
+
 public class AllPaths {
 
     //initializing Adjacency Matrix
-    public class Graph <T> {
+    public int [][]adjacencyMatrix;
+    public int vertices;
 
-        private boolean [][]adjacencyMatrix;
 
-        public Graph (int vertices) {
-            adjacencyMatrix = new boolean[vertices][vertices];
+    //constructor
+    public AllPaths(int vertices) {
+        adjacencyMatrix = new int[vertices][vertices];
+        this.vertices = vertices;
+    }
+    //adds edge in matrix
+    public void addEdge(int i, int j) {
+        adjacencyMatrix[i][j] = 1;
+        //it is a DAG so the connection doesn't go both ways
+    }
+    //prints Adjacency Matrix
+    public void printMatrix(){
+        for (int i = 0; i < vertices; i++){
+            for (int j = 0; j< vertices;j++){
+                System.out.print(adjacencyMatrix[i][j] + " ");
+            }
+            System.out.println();
         }
+    }
+
+    //attempting to use Depth First Search in order to find the rest of the path
+    public void dfs (int source) {
+        boolean [] visited = new boolean[vertices];
+        dfs(source, visited);
+    }
+
+    void dfs(int v, boolean [] visited) {
+        int row = 0;
+        int col = 0;
+        int dRow[] = new int[0];
+        int dCol[] = new int[0];
+
+        Stack s = new Stack();
+
+        visited[v] = true;
+        s.push(v);
+
+        while(!s.empty()) {
+            v = (int) s.pop();
+        }
+        int[] i = adjacencyMatrix[v];
+
+//        for(int z = 0; z < 4; z++)
+//        {
+//            int adjx = row + dRow[z];
+//            int adjy = col + dCol[z];
+//            //s.push(new pair(adjx, adjy));
+//        }
 
     }
-    public int [][]allPaths (int [][]graph){
+
+    //check row 0: if any true values add 0 and index of true val(x) to [][]totalPaths.
+    // then go to x and check if any of are true, if yea, add them...keeo doing this till end of row.
+
+    public int [][]allPaths (int [][] graph){
         //ex: [ [1,2], [3], [3], [] ]
 
-        //graph[index][data] =
+        int [][] totalPaths = new int[4][4];
+        //   ideally: { [0,1,3]  [0,2,3] }
 
-        //thinking: initialize all slots in matrix to 0? or -1 then when I use the in put those with a connection will
-        // turn into 1
+        int totalPathsCounter = 0;
+        //look for any values that are true, if true add to path
 
+        totalPaths[totalPathsCounter] = new int[graph.length];
+        //add 0 to to totalpATH
 
+        for (int rowCounter = 0; rowCounter < adjacencyMatrix.length; rowCounter++){
 
+            for (int columnCounter = rowCounter + 1; columnCounter < adjacencyMatrix.length; columnCounter++) {
 
-
-
-            //Idea/game plan:
-            //1: use Adjacency Matrix to figure out paths and print all the possible ones. By establishing relationship
-            // between noodes. This Matrix should show/give all possible paths
-
-            //      --> note: if I do this one I need a better understanding of whats gonna print out from
-            //          the matrix + how to access the connections or paths(maybe start at beginning and add
-            //          the next "connected" element to a new array matrix that I will return that will contain
-            //          all the paths)
-
+                //logic: if it equals 1 then its true so go to the row of that index (recursion??)
+                if (adjacencyMatrix[rowCounter][columnCounter] == 1) {
+                    dfs(columnCounter); //calling DFS to continue the path but doesn't work as planned
+                    totalPaths[rowCounter][columnCounter] = columnCounter;
 
 
+                }
+            }
+        }
+        return totalPaths;
+        //Idea/game plan:
+        //1: use Adjacency Matrix to figure out paths and print all the possible ones. By establishing relationship
+        // between noodes. This Matrix should show/give all possible paths
 
 
-            //returns a two-dimensional  array with all possible paths from vertex 0 to vertex n-1.
-            return graph;
+    }
+    public static void main(String[] args) {
+
+        int [][] input = { {1,2}, {3}, {3}, {} };
+
+        AllPaths function = new AllPaths(input.length);
+        //index = 0; //then increment
+
+        //input[index,input[][] ]
+        function.addEdge(0,input[0][0]);//adds 1
+
+        function.addEdge(0,input[0][1]);//adds 2
+
+        function.addEdge(1,input[1][0]); //adds 3
+
+        function.addEdge(2,input[2][0]); //adds 3
+
+        function.printMatrix();
+
+        System.out.println(Arrays.deepToString(function.allPaths(input)));
     }
 
 
 }
 
-
-//Ask about github not updating repo:
-//
-// this is what was said in my terminal:
-// git@github.com: Permission denied (publickey).
-//fatal: Could not read from remote repository.
-//
-//Please make sure you have the correct access rights
-//and the repository exists.
-
-
-//2: the long way of adding the index to the front of a new array with the o.g array elements
-//      -->Based on a pattern I'm seeing with the nodes/paths
-//      --> Don't know if its really showing my knowledge of graphs tho...
 
